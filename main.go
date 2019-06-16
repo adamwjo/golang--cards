6623831900907a48666f912e9557f7d57be0b411
adamwjo <adamwjo@gmail.com>
2019-06-15 20:20:38 -0400

    variable declaration

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	// When initializing a variable in Go, the ":=" symbols are used to assign.
+	card := "Ace of Spades"
+
+	// When REassigning only use "="
+	card = "Five of Diamonds"
+
+	fmt.Println(card)
+}
