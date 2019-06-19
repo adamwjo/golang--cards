333975667643dddbd255eca962600c32c08987ad
adamwjo <adamwjo@gmail.com>
2019-06-18 20:02:03 -0400

    commenting code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# golang--cards
+- Golang defines two different data structures when using 'listed' data, the array and slice.
+
+    - The array is a fixed length of items/values
+    - The slice's length is mutable
--- main.go
+++ main.go
@@ -4,10 +4,9 @@ import "fmt"
 
 func main() {
 	// When initializing a variable in Go, the ":=" symbols are used to assign.
-	card := newCard()
 
 	// When REassigning only use "="
-	//
+	card := newCard()
 
 	fmt.Println(card)
 }
