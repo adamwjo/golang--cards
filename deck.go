9dd504473170f4b4ea99683e96995a4b4b184a3a
adamwjo <adamwjo@gmail.com>
2019-06-21 22:05:59 -0400

    newdeck() function

--- deck.go
+++ deck.go
@@ -8,8 +8,16 @@ type deck []string
 func newDeck() deck {
 	cards := deck{}
 
-	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardSuits := []string{" Spades", " Diamonds", " Hearts", " Clubs"}
 	cardValues := []string{"Ace of", "2 of", "3 of", "4 of", "5 of", "6 of", "7 of", "8 of", "9 of", "10 of", "Jack of", "Queen of", "King of"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+suit)
+		}
+	}
+
+	return cards
 
 }
 
